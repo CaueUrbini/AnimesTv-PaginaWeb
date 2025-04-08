9033cf56d7ec7437043b4fb23a4fc68a4c28c565
CaueUrbini <118939698+CaueUrbini@users.noreply.github.com>
2025-04-07 21:18:08 -0300

    Update entrar.js

--- entrar.js
+++ entrar.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
     $("form").submit(function(event){
-        if ($("input[placeholder='Usuário']").val() == "admin")
-        if ($("input[placeholder='Senha']").val() == "1234"){
-            $("span").text("Bem vindo").show().fadeOut(1000);
-            window.open("AnimeTV.html")
+      event.preventDefault();
+        if ($("input[placeholder='Usuário']").val() === "admin" &&
+            $("input[placeholder='Senha']").val() === "1234"){
+            $("span").text("Bem vindo").show().fadeOut(5000);
+            open("AnimeTV.html")
         }
-        if ($("input[placeholder='Usuário']").val() == "admin")
-        if ($("input[placeholder='Senha']").val() != "1234"){
-            $("span").text("Senha inválida").show().fadeOut(1000);
+        else if ($("input[placeholder='Usuário']").val() === "admin" &&
+                 $("input[placeholder='Senha']").val() !== "1234"){
+                 $("span").text("Senha inválida").show().fadeOut(5000);
         }
-        if ($("input[placeholder='Usuário']").val() != "admin")
-        if ($("input[placeholder='Senha']").val() != "1234"){
-        $("span").text("Certifique-se de se Cadastrar!").show().fadeOut(1000);}
-        event.preventDefault();
+        else{
+             $("span").text("Certifique-se de se Cadastrar!").show().fadeOut(5000);
+        }
     });
 });
